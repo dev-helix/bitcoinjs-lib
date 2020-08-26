a19bdfa5c8808e544b494a50fd880dd105ff724a
CryptoDevFr <romain.bergery@gmail.com>
2020-08-26 19:40:36 +0200

    up

--- src/networks.js
+++ src/networks.js
@@ -1,19 +1,19 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
 exports.bitcoin = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'bc',
+  messagePrefix: '\x18DarkNet Signed Message:\n',
+  bech32: 'ph',
   bip32: {
-    public: 0x0488b21e,
-    private: 0x0488ade4,
+    public: 0x022d2533,
+    private: 0x0221312b,
   },
-  pubKeyHash: 0x00,
-  scriptHash: 0x05,
-  wif: 0x80,
+  pubKeyHash: 0x28,
+  scriptHash: 0x0d,
+  wif: 0xd4,
 };
 exports.regtest = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'bcrt',
+  messagePrefix: '\x18DarkNet Signed Message:\n',
+  bech32: 'tp',
   bip32: {
     public: 0x043587cf,
     private: 0x04358394,
@@ -23,8 +23,8 @@ exports.regtest = {
   wif: 0xef,
 };
 exports.testnet = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'tb',
+  messagePrefix: '\x18DarkNet Signed Message:\n',
+  bech32: 'hlixt',
   bip32: {
     public: 0x043587cf,
     private: 0x04358394,
